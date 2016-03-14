9825983870b9c7b072bd60b79fdfa47c3dd889de
Gabriel <mutatisguga@gmail.com>
2016-03-14 14:43:01 -0300

    f

---
+++ Assets/Script/Interface/LoadScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public string sceneName;
+
+    public int tipo;
+
+	void Start ()
+    {
+        if (tipo == 0)
+        {
+            StartCoroutine("GO");
+        }
+	}
+
+    public void Muda()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator GO()
+    {
+        yield return new WaitForSeconds(2);
+        Muda();
+    }
+}
