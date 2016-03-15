afac4a6b4bda890889f9f78cbd95ce3b95247d71
Gabriel <mutatisguga@gmail.com>
2016-03-15 14:04:32 -0300

    f

---
+++ Assets/Script/Manager/Audio/MusicaManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicaManager : MonoBehaviour
+{
+    public AudioSource musica;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        musica.volume = PlayerPrefs.GetFloat("VolumeMusica");
+    }
+}
