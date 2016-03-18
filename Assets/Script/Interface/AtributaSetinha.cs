5078d4eb8f0ea8ced22e1af668c26ac72cc70f9f
Gabriel <mutatisguga@gmail.com>
2016-03-17 23:57:09 -0300

    f

---
+++ Assets/Script/Interface/AtributaSetinha.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtributaSetinha : MonoBehaviour
+{
+    public Animator anim;
+
+    public int tipo;
+
+	void Update ()
+    {
+	    if(tipo == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Joystick1Button11))
+            {
+                anim.SetTrigger("Aperto");
+            }
+        }
+        else if (tipo == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Joystick1Button10))
+            {
+                anim.SetTrigger("Aperto");
+            }
+        }
+    }
+}
---
+++ Assets/Script/Interface/AtributoTroca.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtributoTroca : MonoBehaviour
+{
+    public GameObject[] desliga;
+
+    public GameObject proximo;
+    public GameObject anterior;
+
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Joystick1Button11))
+        {
+            Proximo();
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Joystick1Button10))
+        {
+            Anterior();
+        }
+	}
+
+    void Proximo()
+    {
+        for(int i = 0; i < desliga.Length; i++)
+        {
+            desliga[i].SetActive(false);
+        }
+
+        proximo.SetActive(true);
+    }
+
+    void Anterior()
+    {
+        for (int i = 0; i < desliga.Length; i++)
+        {
+            desliga[i].SetActive(false);
+        }
+
+        anterior.SetActive(true);
+    }
+}
