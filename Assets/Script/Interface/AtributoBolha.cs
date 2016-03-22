2da3eaa459ea9570e10a6b151ab9d90319c58521
Gabriel <mutatisguga@gmail.com>
2016-03-22 13:37:09 -0300

    f

---
+++ Assets/Script/Interface/AtributoBolha.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AtributoBolha : MonoBehaviour
+{
+    public Image[] img;
+
+    public Sprite spt;
+
+    public string nome;
+
+    int x;
+
+    void Update()
+    {
+        x = PlayerPrefs.GetInt(nome);
+
+        for(int i = 0; i < x; i++)
+        {
+            img[i].sprite = spt;
+        }
+    }
+}
