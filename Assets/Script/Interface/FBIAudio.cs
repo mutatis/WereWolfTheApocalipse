37fae7a043aa01c1cff403cd4b12e84254dc531d
Gabriel <mutatisguga@gmail.com>
2016-03-22 17:04:22 -0300

    f

---
+++ Assets/Script/Interface/FBIAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FBIAudio : MonoBehaviour
+{
+    private FMOD.Studio.ParameterInstance levelIntensity;
+    private FMOD.Studio.EventInstance vol;
+
+    [FMODUnity.EventRef]
+    public string som;
+
+    void Start()
+    {
+        vol = FMODUnity.RuntimeManager.CreateInstance(som);
+        vol.start();
+    }
+
+    public void Up()
+    {
+        vol.getParameter("barulho", out levelIntensity);
+        levelIntensity.setValue(10f);
+    }
+
+    public void Down()
+    {
+        vol.getParameter("barulho", out levelIntensity);
+        levelIntensity.setValue(0f);
+    }
+}
