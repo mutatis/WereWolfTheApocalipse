6d0047be0535fd2f1fbb9065bf6855a82dd11b9a
Gabriel <mutatisguga@gmail.com>
2016-03-28 15:46:18 -0300

    Downgrade Unity Bug Fixes Slect Gifts

--- Assets/Script/Manager/SetButtonSkill.cs
+++ Assets/Script/Manager/SetButtonSkill.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SetButtonSkill : MonoBehaviour
 {
+    public Image img;
+
+    public Sprite[] sprt;
+
     public SetGifts gift;
 
     public PaiAtributosEdit meuNumero;
@@ -13,55 +18,63 @@ public class SetButtonSkill : MonoBehaviour
         {
             if(Input.GetKeyDown(KeyCode.Joystick1Button0))
             {
+                img.sprite = sprt[0];
                 PlayerPrefs.SetInt("P1ButtonA", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick1Button1))
             {
+                img.sprite = sprt[1];
                 PlayerPrefs.SetInt("P1ButtonB", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick1Button2))
             {
+                img.sprite = sprt[2];
                 PlayerPrefs.SetInt("P1ButtonX", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick1Button3))
             {
+                img.sprite = sprt[3];
                 PlayerPrefs.SetInt("P1ButtonY", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
         }
         else if (SelectPersonagem.personagem.select2 == meuNumero.meuNumero)
         {
             if (Input.GetKeyDown(KeyCode.Joystick2Button0))
             {
+                img.sprite = sprt[0];
                 PlayerPrefs.SetInt("P2ButtonA", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick2Button1))
             {
+                img.sprite = sprt[1];
                 PlayerPrefs.SetInt("P2ButtonB", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick2Button2))
             {
+                img.sprite = sprt[2];
                 PlayerPrefs.SetInt("P2ButtonX", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick2Button3))
             {
+                img.sprite = sprt[3];
                 PlayerPrefs.SetInt("P2ButtonY", gift.skill);
-                gift.atributo.enabled = true;
-                gift.select.SetActive(false);
+                StartCoroutine("GO");
             }
         }
+    }
+
+    IEnumerator GO()
+    {
+        yield return new WaitForSeconds(1);
+        gift.atributo.enabled = true;
+        gift.select.SetActive(false);
+
     }
 }
