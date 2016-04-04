89766556bd203a2f69f6483cb7bc286caf8d333e
Gabriel <mutatisguga@gmail.com>
2016-04-04 15:46:20 -0300

    Dinamic Interface Jogo

---
+++ Assets/Script/Interface/InterfaceLifePlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceLifePlayer : MonoBehaviour
+{
+    public GameObject[] obj;
+
+    void Start()
+    {
+        for(int i = 0; i < PlayerPrefs.GetInt("Players"); i++)
+        {
+            obj[i].SetActive(true);
+        }
+    }
+}
