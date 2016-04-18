8da44175efbbb6b4383e97d589e6bd3cda667775
Gabriel <mutatisguga@gmail.com>
2016-04-18 16:30:55 -0300

    f

---
+++ Assets/Script/Manager/FollowTarget.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target; // Reference to the player.
+
+	public Vector3 offset;   // The offset at which the Health Bar follows the player.
+
+    public GameObject[] obj;
+
+    public float smoothTime = 0.3f; //Makes this behaviour smooth
+    public float[] pos;
+    
+    public int cont;
+
+    public bool segue;
+
+    float num;
+    private float xPosition; //wanted X position
+    private float yPosition; //wanted Y position
+
+    private Vector3 velocity = Vector3.zero; //A reference value used by SmoothDamp that tracks this object velocity
+	
+	void FixedUpdate ()
+	{
+        num = transform.position.x;
+        if(num >= pos[cont])
+        {
+            segue = false;
+            obj[cont].SetActive(true);
+        }
+
+        if (segue)
+        {
+            xPosition = target.position.x + offset.x;
+            yPosition = offset.y;
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(xPosition, transform.position.y, transform.position.z), ref velocity, smoothTime);
+        }
+	}
+}
--- Assets/Script/Manager/TimeCreator.cs
+++ Assets/Script/Manager/TimeCreator.cs
@@ -3,19 +3,37 @@ using System.Collections;
 
 public class TimeCreator : MonoBehaviour
 {
+    public FollowTarget camera;
 
     public SpriteRowCreator creator;
 
+    public Vector2 quant;
+
+    int num;
+
+    int x;
+
 	void Start ()
     {
+        num = Random.Range((int)quant.x, (int)quant.y);
         StartCoroutine("GO");
 	}
 
     IEnumerator GO()
     {
         yield return new WaitForSeconds(4);
-        creator.CreateSprites();
-        Denovo();
+        if (x > num)
+        {
+            camera.cont++;
+            camera.segue = true;
+            creator.enabled = false;
+        }
+        else
+        {
+            x++;
+            creator.CreateSprites();
+            Denovo();
+        }
     }
 
     void Denovo()
