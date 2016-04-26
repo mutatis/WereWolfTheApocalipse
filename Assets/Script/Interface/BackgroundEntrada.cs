31fcad5c7d8bba9ec9a9dded49bdecfe78ea371c
Gabriel <mutatisguga@gmail.com>
2016-04-26 11:57:05 -0300

    f

---
+++ Assets/Script/Interface/BackgroundEntrada.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundEntrada : MonoBehaviour
+{
+    public GameObject obj;
+
+    public void Play()
+    {
+        obj.SetActive(true);
+    }
+}
