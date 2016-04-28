2117ff5afa9cc887050024b224e30a1427d763a7
Gabriel <mutatisguga@gmail.com>
2016-04-28 10:44:09 -0300

    g

--- Assets/Script/Interface/BackgroundEntrada.cs
+++ Assets/Script/Interface/BackgroundEntrada.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class BackgroundEntrada : MonoBehaviour
 {
+    public Animator[] anim;
+
     public GameObject obj;
 
     public void Play()
     {
         obj.SetActive(true);
+        for(int i = 0; i < anim.Length; i++)
+        {
+            anim[i].enabled = true;
+        }
     }
 }
