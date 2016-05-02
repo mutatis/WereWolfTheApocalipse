debf90606c10f0cf213ca75e6edf5c1a18f6587b
Gabriel <mutatisguga@gmail.com>
2016-05-02 13:21:26 -0300

    v

--- Assets/Script/Manager/SetButtonSkill.cs
+++ Assets/Script/Manager/SetButtonSkill.cs
@@ -24,25 +24,25 @@ public class SetButtonSkill : MonoBehaviour
             if(Input.GetKeyDown(KeyCode.Joystick1Button0))
             {
                 img.sprite = sprt[0];
-                PlayerPrefs.SetInt(meuNumero + "P1ButtonA", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P1ButtonA", gift.skill);
                 StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick1Button1))
             {
                 img.sprite = sprt[1];
-                PlayerPrefs.SetInt(meuNumero + "P1ButtonB", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P1ButtonB", gift.skill);
                 StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick1Button2))
             {
                 img.sprite = sprt[2];
-                PlayerPrefs.SetInt(meuNumero + "P1ButtonX", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P1ButtonX", gift.skill);
                 StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick1Button3))
             {
                 img.sprite = sprt[3];
-                PlayerPrefs.SetInt(meuNumero + "P1ButtonY", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P1ButtonY", gift.skill);
                 StartCoroutine("GO");
             }
         }
@@ -51,25 +51,25 @@ public class SetButtonSkill : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Joystick2Button0))
             {
                 img.sprite = sprt[0];
-                PlayerPrefs.SetInt(meuNumero + "P2ButtonA", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P2ButtonA", gift.skill);
                 StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick2Button1))
             {
                 img.sprite = sprt[1];
-                PlayerPrefs.SetInt(meuNumero + "P2ButtonB", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P2ButtonB", gift.skill);
                 StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick2Button2))
             {
                 img.sprite = sprt[2];
-                PlayerPrefs.SetInt(meuNumero + "P2ButtonX", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P2ButtonX", gift.skill);
                 StartCoroutine("GO");
             }
             else if (Input.GetKeyDown(KeyCode.Joystick2Button3))
             {
                 img.sprite = sprt[3];
-                PlayerPrefs.SetInt(meuNumero + "P2ButtonY", gift.skill);
+                PlayerPrefs.SetInt(meuNumero.nome + "P2ButtonY", gift.skill);
                 StartCoroutine("GO");
             }
         }
