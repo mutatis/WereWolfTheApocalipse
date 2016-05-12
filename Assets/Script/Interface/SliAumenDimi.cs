7cb21f567a95b8c8ac7cf169417b7a6f90ecf4e2
Gabriel <mutatisguga@gmail.com>
2016-05-12 13:53:02 -0300

    slamduk

---
+++ Assets/Script/Interface/SliAumenDimi.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliAumenDimi : MonoBehaviour
+{
+    public Slider sli;
+
+    public float maxSli, temp, x;
+
+    void Start()
+    {
+        maxSli = sli.maxValue;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Joystick1Button0) && temp >= 0.45f && temp <= 0.65f)
+        {
+            sli.gameObject.SetActive(false);
+        }
+
+        if(x == 1)
+        {
+            temp -= 0.05f;
+        }
+        else
+        {
+            temp += 0.05f;
+        }
+
+        sli.value = temp;
+
+        if(temp >= maxSli)
+        {
+            x = 1;
+        }
+        else if(temp <= 0)
+        {
+            x = 0;
+        }
+    }
+}
---
+++ Assets/Script/Player/PackAtivaSlamDunk.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PackAtivaSlamDunk : MonoBehaviour
+{
+    public Slider sliPunhe, sliAumenDimi;
+
+    public Transform segue, pai;
+
+    public int numPress;
+
+    int contador, etapa;
+
+    void Start()
+    {
+        sliPunhe.maxValue = numPress;
+    }
+
+    void Update()
+    {
+        //segue.position = new Vector3(pai.position.x, pai.position.y + 3, pai.position.z);
+        if(Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            if (etapa == 0)
+            {
+                contador++;
+            }
+            sliPunhe.value = contador;
+        }
+
+        if(contador >= numPress && etapa == 0)
+        {
+            sliPunhe.gameObject.SetActive(false);
+            sliAumenDimi.gameObject.SetActive(true);
+            etapa = 1;
+        }
+    }
+}
