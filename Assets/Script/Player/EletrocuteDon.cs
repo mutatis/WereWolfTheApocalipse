f6d060959e8372d377e920c93ec212942ff93423
Gabriel <mutatisguga@gmail.com>
2016-07-19 14:26:10 -0300

    g

--- Assets/Script/Player/EletrocuteDon.cs
+++ Assets/Script/Player/EletrocuteDon.cs
@@ -13,21 +13,22 @@ public class EletrocuteDon : MonoBehaviour
     GameObject obj;
 
     void Start()
-    {
+	{
+		audioInstance = FMODUnity.RuntimeManager.CreateInstance(raio);
+		audioInstance.setVolume(PlayerPrefs.GetFloat("VolumeFX"));
+		audioInstance.start();
         StartCoroutine("GO");
     }
 
     IEnumerator GO()
     {
-        yield return new WaitForSeconds(2);
+		yield return new WaitForSeconds(2);
+		audioInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         Destroy(gameObject);
     }
 
     void Dano(GameObject other)
     {
-        audioInstance = FMODUnity.RuntimeManager.CreateInstance(raio);
-        audioInstance.setVolume(PlayerPrefs.GetFloat("VolumeFX"));
-        audioInstance.start();
             if (other.gameObject.tag == "Enemy")
             {
                 other.gameObject.GetComponent<EnemyController>().Dano(dmg, false, gameObject);
