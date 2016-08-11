ff8d3c324e92a68d88f8f810644bc6033356f211
Gabriel <mutatisguga@gmail.com>
2016-08-11 11:52:22 -0300

    pacman

---
+++ Assets/Script/Pacman/PacmanGame.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacmanGame : MonoBehaviour
+{
+    public float velocity;
+
+    float x, z;
+
+    bool up, down, right, left;
+
+	void Update ()
+    {
+        transform.Translate(x, 0, z, Space.World);
+
+        if (Input.GetAxisRaw("DpadXP1") > 0)
+        {
+            right = true;
+            left = false;
+            up = false;
+            down = false;
+        }
+        else if(Input.GetAxisRaw("DpadXP1") < 0)
+        {
+            right = false;
+            left = true;
+            up = false;
+            down = false;
+        }
+        else if (Input.GetAxisRaw("DpadYP1") < 0)
+        {
+            right = false;
+            left = false;
+            up = false;
+            down = true;
+        }
+        else if (Input.GetAxisRaw("DpadYP1") > 0)
+        {
+            right = false;
+            left = false;
+            up = true;
+            down = false;
+        }
+
+        if(right)
+        {
+            x = velocity;
+            z = 0;
+        }
+        else if(left)
+        {
+            x = velocity * -1;
+            z = 0;
+        }
+        else if(up)
+        {
+            z = velocity;
+            x = 0;
+        }
+        else if(down)
+        {
+            z = velocity * -1;
+            x = 0;
+        }
+    }
+}
