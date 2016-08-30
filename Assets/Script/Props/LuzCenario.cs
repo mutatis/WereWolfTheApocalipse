b2097f2f29fd98035893e4caf626d8877ac29191
Gabriel <mutatisguga@gmail.com>
2016-08-30 14:22:49 -0300

    gg

---
+++ Assets/Script/Props/LuzCenario.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LuzCenario : MonoBehaviour
+{
+    public Vector2 minMax;
+
+    public Animator anim;
+
+    void Start()
+    {
+        StartCoroutine("Pisca");
+    }
+
+    IEnumerator Pisca()
+    {
+        yield return new WaitForSeconds(Random.Range(minMax.x, minMax.y));
+        anim.SetTrigger("Pisca");
+        Volta();
+    }
+
+    void Volta()
+    {
+        StopCoroutine("Pisca");
+        StartCoroutine("Pisca");
+    }
+}
