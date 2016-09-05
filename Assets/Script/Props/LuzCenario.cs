c622bff2abea1371461861f95beba2db38be1da4
Gabriel <mutatisguga@gmail.com>
2016-09-05 13:27:57 -0300

    a

--- Assets/Script/Props/LuzCenario.cs
+++ Assets/Script/Props/LuzCenario.cs
@@ -7,14 +7,26 @@ public class LuzCenario : MonoBehaviour
 
     public Animator anim;
 
+    [FMODUnity.EventRef]
+    public string luz;
+
+    FMOD.Studio.EventInstance luzInstance;
+
     void Start()
     {
+        luzInstance = FMODUnity.RuntimeManager.CreateInstance(luz);
+        luzInstance.setVolume(PlayerPrefs.GetFloat("VolumeFX"));
+        luzInstance.start();
         StartCoroutine("Pisca");
     }
 
     IEnumerator Pisca()
     {
         yield return new WaitForSeconds(Random.Range(minMax.x, minMax.y));
+        luzInstance = FMODUnity.RuntimeManager.CreateInstance(luz);
+        luzInstance.setVolume(PlayerPrefs.GetFloat("VolumeFX"));
+        luzInstance.start();
+        yield return new WaitForSeconds(0.5f);
         anim.SetTrigger("Pisca");
         Volta();
     }
